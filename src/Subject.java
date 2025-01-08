8fa9ab188d0bd6e99eef181c55e0ab31e1694e8a
sreng.itc <sreng@itc.edu.kh>
2025-01-08 15:28:54 +0700

    Initial project setup

---
+++ src/Subject.java
@@ -0,0 +1,33 @@
+public class Subject {
+    private String name;
+    private int credits;
+
+    public Subject(String name, int credits) {
+        this.name = name;
+        this.credits = credits;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    @Override
+    public String toString() {
+        return "Subject{" +
+                "name='" + name + '\'' +
+                ", credits=" + credits +
+                '}';
+    }
+}
